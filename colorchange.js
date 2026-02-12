7316c79da7d13ac29c212d4d5d8a4408de9797f6
Munge-flip <ggonzales2@ssct.edu.ph>
2026-02-12 15:32:31 +0800

    commit

---
+++ colorchange.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+
+const GridBox = () => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  return (
+    <TouchableOpacity 
+      // 2. SHORTCUT: Use an array [] to mix static styles with dynamic styles
+      style={[styles.grids, { backgroundColor: isPressed ? 'blue' : 'red' }]} 
+      onPress={() => setIsPressed(!isPressed)}
+    >
+      <Text style={styles.buttonText}>Grid</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View style={styles.gridContainer}>
+        <GridBox />
+        <GridBox />
+        <GridBox />
+        <GridBox />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 100,
+    gap: 10,
+    marginBottom: 10,
+  },
+  grids: {
+    borderWidth: 1,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
