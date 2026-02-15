dda7da848f4f55a671aad3115c2dda7192923dfd
Munge-flip <ggonzales2@ssct.edu.ph>
2026-02-15 11:43:28 +0800

    commit

--- user.js
+++ user.js
@@ -8,47 +8,47 @@ export default function App() {
   })
   const [userSubmit, setUserSubmit] = useState(null);
 
-  const handleUser = (id, info) => {
-    setUserData({...userData, [id]: info})
+  const handleUserInputs = (id, data) => {
+    setUserData({...userData, [id]: data})
   }
-  const handleUserSubmission = () => {
+  const handleUserSubmissions = () => {
     setUserSubmit(userData);
   }
-  const isUsernameValid = userData.password.length >= 8;
-  const isPasswordValid = userData.username.length >= 8;
+  const isUsernameValid = /\d/.test(userData.username) && userData.username.length >= 8;
+  const isPasswordValid = /\d/.test(userData.password) && userData.password.length >= 8;
   const isFormFilled = userData.username !== '' && userData.password !== '';
   const canSubmit = isFormFilled && isUsernameValid && isPasswordValid;
-
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <TextInput style={styles.paragraph}
           placeholder="Username: "
           value={userData.username}
-          onChangeText={(t) => handleUser('username', t)}
+          onChangeText={(t) => handleUserInputs('username', t)}
         />
         {!isUsernameValid && userData.username.length > 0 && (
-          <Text style={{color: 'red'}}>Minimum of 8 characters</Text>
+          <Text>Username needs to be minimum of 8 characters and has atleast 1 number in it</Text>
         )}
         <TextInput style={styles.paragraph}
           placeholder="Password: "
           value={userData.password}
-          onChangeText={(t) => handleUser('password', t)}
-          secureTextEntry={true}
+          onChangeText={(t) => handleUserInputs('password', t)}
         />
         {!isPasswordValid && userData.password.length > 0 && (
-          <Text style={{color: 'red'}}>Minimum of 8 characters</Text>
+          <Text>Username needs to be minimum of 8 characters and has atleast 1 number in it</Text>
         )}
-        <Button style={styles.paragraph}
-          title="Submit"
-          onPress={handleUserSubmission}
-          disabled={!canSubmit}
+        <Button 
+        title="Register"
+        onPress={handleUserSubmissions}
+        disabled={!canSubmit}
         />
       </View>
-
       {userSubmit && (
         <View style={styles.paragraph}>
+          <Text>You have successfully registered</Text>
+          <Text>Here are your info:</Text>
           <Text>Username: {userData.username} </Text>
+          <Text>Password: {userData.password} </Text>
         </View>
       )}
     </SafeAreaView>
@@ -58,11 +58,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
   paragraph: {
-    margin: 5,
+    margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
   },
